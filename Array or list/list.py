2952f10e71192b7cc11176239814ccd08bd1b4fe
Nazhat Afza Zain <nazhatafza1@gmail.com>
2023-10-14 00:47:07 +0700

    array or list

---
+++ Array or list/list.py
@@ -0,0 +1,27 @@
+# jika ingin menggunakan array harus meng-import array terlebih dahulu
+import array
+
+# list
+x = [1, 2, 3, 4]
+print(x)
+
+# array
+x = array.array("i", [1, 2, 3, 4, 5])
+print(x)
+print(type(x))
+
+# index dimulai dari 0
+siswa = [10, 100, 50, 85, 90, 40]
+print(siswa)
+
+# mengambil index ke - 0
+print(siswa[0])
+
+# elemen -> menghasilkan lokasi memori setiap elemen pada list
+var_list = [1, 2, 3]
+for elemen in var_list:
+    print(id(elemen))
+
+# define array value
+var_arr = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+print(var_arr)
--- Control Flow/quiz.py
+++ Control Flow/quiz.py
@@ -3,3 +3,14 @@ for i in range(501):
     if i % 2 == 0:
         evenNumber.append(i)
 print(evenNumber)
+
+
+i = 11
+if i < 10:
+    print(i)
+else:
+    print("nilai")
+
+lulus = True
+
+print("dicoding") if lulus else print("py")
