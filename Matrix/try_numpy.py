8480a377491db1e7c136d4e27c8e6461b87590f4
Nazhat Afza Zain <nazhatafza1@gmail.com>
2023-10-14 21:47:36 +0700

    akses elemen dari matrix

--- Matrix/try_numpy.py
+++ Matrix/try_numpy.py
@@ -11,3 +11,23 @@ print(
     f"ukuran elemen list dalam byte: {sys.getsizeof(var_list)*len(var_list)} bytes")
 print(
     f"ukuran elemen array using numpy dalam byte: {var_array.size*var_array.itemsize}")
+
+# Implementasi matriks dengan deklarasi dan inisialisasi
+matrix = [[1, 0, 1, 0, 1],
+          [0, 1, 0, 1, 0],
+          [1, 0, 1, 0, 1]]  # insialisasi matriks satuan baris = 3, kolom 5
+print(matrix)
+
+# deklarasi matriks default
+# range pertama merupakan baris, dan range kedua merupakan kolom
+matrix_def = [[0 for j in range(5)] for i in range(3)]
+print(matrix_def)
+
+# akses elemen matriks
+
+print("mengakses matrix")
+var_mat = [[1, 2, 3, 4],
+           [5, 6, 7, 8],
+           [9, 10, 11, 12]]
+
+print(var_mat[0][1])  # -> mengakses pada baris/index ke 0 dan kolom ke-1
