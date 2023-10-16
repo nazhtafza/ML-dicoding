deaebfc39ba4d71c4776bb3f1ef20a951cfafadf
Nazhat Afza Zain <nazhatafza1@gmail.com>
2023-10-16 18:04:34 +0700

    learn function

---
+++ function/Fungsi.py
@@ -0,0 +1,11 @@
+# contoh function
+def rectangle(panjang, lebar):
+    luas_rectangle = panjang*lebar
+    return luas_rectangle
+
+
+rectangle1 = rectangle(10, 5)
+print(f" luas persegi panjang 1 {rectangle1}")
+
+rectangle2 = rectangle(20, 2)
+print(f" luas persegi panjang 2 {rectangle2}")
