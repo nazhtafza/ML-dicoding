100460a57a93d963040f2d67b94e217ac9d99ddc
Nazhat Afza Zain <nazhatafza1@gmail.com>
2023-10-16 18:43:20 +0700

    try make luas lingkaran with funct

--- function/Fungsi.py
+++ function/Fungsi.py
@@ -1,4 +1,8 @@
-# contoh function
+# create direktori and then import function
+from funct_lingkaran import lingkaran, kel_lingkaran
+# contoh user defined function
+
+
 def rectangle(panjang, lebar):
     luas_rectangle = panjang*lebar
     return luas_rectangle
@@ -9,3 +13,10 @@ print(f" luas persegi panjang 1 {rectangle1}")
 
 rectangle2 = rectangle(20, 2)
 print(f" luas persegi panjang 2 {rectangle2}")
+
+# print result luas lingkaran
+lingkaran1 = lingkaran(30)
+print(f"luas lingkaran adalah: {lingkaran1}")
+
+lingkaran_kel = kel_lingkaran(30)
+print(f"keliling lingkaran: {lingkaran_kel}")
---
+++ function/funct_lingkaran.py
@@ -0,0 +1,11 @@
+# luas lingkaran
+def lingkaran(jari_jari):
+    luas_lingkaran = 3.14 * jari_jari**2
+    return luas_lingkaran
+
+# keliling lingkaran
+
+
+def kel_lingkaran(jari_jari):
+    keliling = 2*3.14*jari_jari
+    return keliling
