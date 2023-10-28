b118b0b9e81e2fe9334df742161d7eec88624594
Nazhat Afza Zain <nazhatafza1@gmail.com>
2023-10-28 21:05:56 +0700

    [done] quiz basic python

---
+++ quiz.py
@@ -0,0 +1,11 @@
+x = 1960
+x += 901
+print(x)
+
+nilai = 68
+if nilai >= 80:
+    print("Selamat! Anda mendapat nilai A")
+elif nilai >= 60:
+    print("Hmm.. Anda mendapat nilai B")
+else:
+    print("Waduh, Anda mendapat nilai C")
